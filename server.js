b6d35d99e2876b6210e7b7aa056265b59cdc4aa3
Evgene Sotnikov <esotnikov037@gmail.com>
2019-08-24 14:59:38 +0300

    fix

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/github-finder'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/github-finder/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 app.listen(process.env.PORT || 8080);
